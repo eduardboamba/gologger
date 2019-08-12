daaaa5cecbcf5ccdedbd4cdc366df6e2a473251e
Eduard Boamba <eduard-andrei.boamba@sap.com>
2019-08-12 11:58:51 +0200

    add some sample logging statements

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "github.com/eduardboamba/gologger/pkg/util/logger"
 
 func main() {
-	logger.Info()
+	logger.Info("this app is running great")
+
+	logger.Debug("got some debugging stuff logged here")
+
+	logger.Error("something might have gone wrong")
+
+	logger.Fatal("oops, fatality...")
 }
